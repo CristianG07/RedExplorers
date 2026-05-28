970916224f9f4ae35ab4813d5e649d7241d16278
Cristian Gonzalez <crisgonzalez07g@gmail.com>
2026-05-28 15:07:13 -0500

    fix

--- src/components/ui/AsideSwiper.jsx
+++ src/components/ui/AsideSwiper.jsx
@@ -23,6 +23,13 @@ export default function AsideSwiper() {
                   <h4 className="text-3xl md:text-5xl lg:text-4xl font-bold leading-12">Finding Love & home in Tbilisi, Georgia</h4>
                </div>
             </SwiperSlide>
+            <SwiperSlide>
+               <div className="h-full flex flex-col gap-16 p-6 text-white bg-primary/30 ">
+                  <img className="absolute top-0 left-0 h-full w-full object-fill -z-10" src={car_beach} alt="car image" />
+                  <span className="text-xl md:text-2xl">September 17, 2018 - Tips & Tricks</span>
+                  <h4 className="text-3xl md:text-5xl lg:text-4xl font-bold leading-12">Finding Love & home in Tbilisi, Georgia</h4>
+               </div>
+            </SwiperSlide>
             <div className="absolute bottom-12 left-6 flex gap-5 z-20">
                <ButtonSwiper btnType="prev">
                   <img src={arrow_left} alt="arrow left icon" />
